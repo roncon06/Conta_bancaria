f5677567cdd7d3995156aa5ce6cd16c269294d5b
Roncon <lucasroncon16@gmail.com>
2024-09-02 20:17:18 -0300

    criacao app conta bancaria

---
+++ android/app/src/main/kotlin/com/example/appbanco/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.appbanco
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:appbanco/widget/myapp.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
